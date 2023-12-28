23dc2b14800ab46d071ba91f0a46874734c271f3
Cole Johnson <coledjohn@gmail.com>
2023-12-27 23:35:59 -0700

    Making the solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,26 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    // File hosts.txt must exist in the current path
+    // file example.txt must exist in current path
     if let Ok(lines) = read_lines("./example.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
-            if let Ok(ip) = line {
-                println!("{}", ip);
+            if let Ok(l) = line {
+                println!("{}", l);
+                // create a string that can hold the two digits
+                let mut num_string = String::with_capacity(2);
+                for char in l.chars() {
+                    if char.is_numeric() {
+                        num_string.insert(0, char);
+                    }
+                }
+                for char in l.chars().rev() {
+                    if char.is_numeric() {
+                        num_string.insert(1, char);
+                    }
+                }
+                let num = num_string.parse::<i32>().unwrap();
+                println!("Number parsed from line: {}", num);
             }
         }
     }
