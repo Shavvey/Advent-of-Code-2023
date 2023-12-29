33af0558885e48a1fa966328d1745b997d3de456
Cole Johnson <coledjohn@gmail.com>
2023-12-28 20:31:07 -0700

    Its ugly, but decoding the first number now works

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,13 +8,13 @@ use std::{
 };
 
 fn main() {
-    println!("hello world!");
+    parse_num_digits();
 }
 
-fn parse_num_digits() -> i32 {
+fn parse_num_digits() {
     // filename that we willl read from
-    let filename = "example.txt";
-    let sum: i32 = 0;
+    let filename = "input.txt";
+    let mut num1: i32 = -1;
     println!("Reading from the file: {filename}");
     // consume the iterator and returns (optionally) a string
     if let Ok(lines) = read_lines(filename) {
@@ -39,36 +39,53 @@ fn parse_num_digits() -> i32 {
                 let mut num_string = String::new();
                 // clone a mutable reference to dictionary
                 let mut num_dictionary = word_num.clone();
-                let mut num1: i32;
-                let mut num2: i32;
                 // create character iterator using each char
                 for char in ln.chars() {
+                    if char.is_digit(10) {
+                        match char.to_digit(10) {
+                            Some(val) => {
+                                num1 = val as i32;
+                                break;
+                            }
+                            // if a value cannot be matched do nothing
+                            None => (),
+                        }
+                    }
                     num_string.push(char);
 
                     for key in word_num.keys() {
                         if !key.contains(&num_string) {
                             num_dictionary.remove(key);
                         }
-                        if num_dictionary.len() == 1 {
-                            match num_dictionary.get(key) {
-                                Some(val) => {
-                                    num1 = *val;
-                                    // break from loop
+                    }
+                    if num_dictionary.is_empty() {
+                        // clear out num string
+                        num_string.clear();
+                        // copy word number dictionary again
+                        num_dictionary = word_num.clone();
+                    }
+
+                    if num_dictionary.len() == 1 {
+                        // this is bad and doesn't work
+                        match num_dictionary.iter().last() {
+                            Some(dict) => {
+                                // if the num_string is matched with dictionary
+                                if dict.0.eq(&num_string) {
+                                    // save the value in dict as the decoded number
+                                    num1 = *dict.1;
+                                    // break out of the iteration loop
                                     break;
                                 }
-                                // if we can't get they key yet just do nothing
-                                None => (),
                             }
-                        }
-                        if num_dictionary.len() == 0 {
-                            num_string.clear();
+                            // if dictionary cannot be matched yet do nothing
+                            None => (),
                         }
                     }
                 }
+                println!("number parsed from line: {num1}");
             }
         }
     }
-    sum
 }
 
 // read each line using a buffer reader instead of storing the whole file as
