3228abac174a42733000285da4a315c952b12c52
Cole Johnson <coledjohn@gmail.com>
2024-01-14 11:07:41 -0700

    slowly working on day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+enum Color {
+    Blue,
+    Green,
+    Red,
+}
+
+struct Cubes {
+    color: Color,
+    quantity: u32,
+}
+
+struct Game {
+    // array that contains cubes revealed in each round
+    round_one: [Cubes; 3],
+    round_two: [Cubes; 3],
 }
