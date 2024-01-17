1f908dc7afff13bb5c2af0195c04b592cbe242fa
Cole Johnson <coledjohn@gmail.com>
2024-01-17 09:57:07 -0700

    working on day2 some more

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {}
 
+// enum type that represents the possible color the cubes may have
 enum Color {
     Blue,
     Green,
@@ -15,4 +16,5 @@ struct Game {
     // array that contains cubes revealed in each round
     round_one: [Cubes; 3],
     round_two: [Cubes; 3],
+    // how many red, blue, and green cubes are revealed
 }
