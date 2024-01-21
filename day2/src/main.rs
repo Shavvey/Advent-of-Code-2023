823679d04236155a8fddfe0fe52264936f72b2a5
Cole Johnson <coledjohn@gmail.com>
2024-01-21 14:26:56 -0700

    Adding `read_lines` function to begin parsing the textfile

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,10 @@
+#![allow(dead_code)]
+
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
 // enum type that represents the possible color the cubes may have
 enum Color {
     Blue,
@@ -16,8 +23,17 @@ struct Game {
     round_two: [Cubes; 3],
     // how many red, blue, and green cubes are revealed
 }
+// function to open a file and start reading the lines of it
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file: File = File::open(filename)?;
+    // construct buffer reader from opened file
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
     println!("Hello, World!");
     // enum type can be declared something like this
-    let color: Color = Color::Blue;
 }
